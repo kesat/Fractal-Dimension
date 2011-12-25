fd7fb41676903ca6db02e490dc4faf310952b43e
Kıvanç Esat <kivancesat@gmail.com>
2011-12-25 02:52:23 +0200

    version 0.2.1 Finds fractal dimention of a given image using box counting method

---
+++ runner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 24 22:11:44 2011
+44
+@author: busker
+"""
+
+import numpy as np
+import fracdim as fr
+
+image = np.ones((512,512))
+fr.plot_size_number(image)
