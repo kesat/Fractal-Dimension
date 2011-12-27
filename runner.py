4c960e89de387ce0a6e04f4e972f37aa9509f793
Kıvanç Esat <kivancesat@gmail.com>
2011-12-27 14:26:51 +0200

    Runner converts the image that has been interpreted in the fracdim to the binary matrix form.

--- runner.py
+++ runner.py
@@ -7,6 +7,24 @@ Created on Sat Dec 24 22:11:44 2011
 
 import numpy as np
 import fracdim as fr
+import Image
 
-image = np.ones((512,512))
-fr.plot_size_number(image)
+#image = np.ones((512,512))
+#image =  pl.Image.open('/home/busker/Documents/Fractal-Dimention/Test_images/Norway_municipalities_fdim-1.52__treshold_512-512.png')
+
+
+#fr.slope_finder(image)
+
+def image_converter (image_pattern):
+    image = Image.open(image_pattern)
+    image_binary = image.convert('1')
+    image_binary_list= list(image_binary.getdata())
+    image_binary_matrix = (np.resize(image_binary_list, image_binary.size)!=255)*1 #thanks to mali
+    
+    return image_binary_matrix
+
+image = image_converter ('Test_images/Sierpinski_gasket.png')
+slope =  fr.slope_finder(image)
+
+print slope
+    
