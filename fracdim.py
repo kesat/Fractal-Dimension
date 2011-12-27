261cfd71a0a77af6359bc5ea1c071debfb93a1e3
Kıvanç Esat <kivancesat@gmail.com>
2011-12-27 12:16:29 +0200

    Version 0.1.4
    Though this is the first working version of the module still has got problematic issues:
    1) The image that's Hausdorff dimension wanted to be found should be square and the size should be a factor of two.
    2) The fractal dimension of the Sierpinski triangle supposed to be 1.5849 but that comes out as 1.9853 from the program.

--- fracdim.py
+++ fracdim.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 19 00:02:31 2011
-
-@author: busker
-"""
-
+# Fractal Dimension Calculator
 __author__ = "Kivanc Esat"
 __copyright__ = ""
-__credits__ = ["Kivanc Esat, Mehmet Ali Anıl"]
+__credits__ = ["Kivanc Esat, Mehmet Ali Anil"]
 __license__ = ""
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __maintainer__ = "Kivanc Esat"
 __email__ = "esat [at] itu.edu.tr"
 __status__ = "Production"
@@ -23,32 +17,36 @@ import matplotlib.pyplot as plt
 def box_counter (image, box_size):
     """
     """
-    image_size = len(image) 
-    total_box_number = image_size / box_size
-    reduced = np.zeros((total_box_number, total_box_number))
+    image_size = len(image)   # ONLY WORKS WITH SQUARE IMAGE
+    print image_size    
+    total_box_number = (image_size / box_size) # ONLY WORKS WITH SQUARE IMAGE
+    reduced = np.zeros((total_box_number, total_box_number)) # ONLY WORKS WITH SQUARE IMAGE
     
-    for row in range(0,image_size,box_size):
-       for col in range(0,image_size,box_size):
-        
+    for row in range(0,image_size,box_size):  # ONLY WORKS WITH SQUARE IMAGE
+       for col in range(0,image_size,box_size): # ONLY WORKS WITH SQUARE IMAGE
+         
             sum_m = 0           
             
-            for box_row in range(row,(box_size+row)):
-                for box_col in range(col,(box_size+col)):
+            for box_row in range(row,(box_size+row)): 
+                for box_col in range(col,(box_size+col)):  
 
                     sum_m = sum_m + image[box_row, box_col]
-  
+            print sum_m
+            print row,col
+            print box_row, box_col
             if sum_m >= (box_size**2) / 2:
                 reduced_row = row/box_size
                 reduced_col = col/box_size
                 reduced[reduced_row, reduced_col] = 1
-
+    print reduced
     reduced_box_number = np.sum(reduced)
+    print reduced_box_number    
     return reduced_box_number
 
 def box_size_iterator (image):
     """ 
     """
-    list_box_size =[4,8,16,32,64,128,512]
+    list_box_size =[4,8,16]
     #list_box_size = np.zeros((n-2))
     #for (li_ctr, li) in enumerate(range(2,n)):
       #  list_box_size[li_ctr] = 2**li
@@ -57,21 +55,20 @@ def box_size_iterator (image):
 
     for (box_size_ctr, box_size) in enumerate(list_box_size):
         list_box_number[box_size_ctr]  = box_counter(image, box_size)
-    
     return list_box_number, list_box_size
 
-def plot_size_number (image):
+def slope_finder (image):
     """
     """
     list_of_all = box_size_iterator(image)
     list_box_number = list_of_all[0]
     list_box_size = list_of_all[1]
-   
+    print list_box_size, list_box_number
     slope = np.polyfit(np.log(list_box_size),np.log(list_box_number),1)
     print slope
-    #plt.loglog(list_box_size,list_box_number)
-    #plt.xlabel('Box number'); plt.ylabel('Box number')
-    #plt.yscale('log'); plt.xscale('log')
-    #plt.show()
+    plt.loglog(list_box_size,list_box_number, 'r')
+    plt.xlabel('Box size'); plt.ylabel('Box number')
+    plt.yscale('log'); plt.xscale('log')
+    plt.show()
         
     
